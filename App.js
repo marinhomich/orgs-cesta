6c74638250f009fb8db654d815642b6f03930ffa
Michel Marinho <michel.marinho04@outlook.com>
2022-12-22 16:53:14 -0400

    Add: Detalhes da Cesta

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar, SafeAreaView, View} from 'react-native';
+import Cesta from "./src/pages/cesta";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+     <View>
+         <StatusBar/>
+         <Cesta/>
+     </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
