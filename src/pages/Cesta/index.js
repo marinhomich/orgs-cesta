2981572d6802d22e6d68b7b50360ca3c770196e9
Michel Marinho <michel.marinho04@outlook.com>
2022-12-23 11:08:33 -0400

    Add: Items List

--- src/pages/Cesta/index.js
+++ src/pages/Cesta/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-import {StyleSheet, View} from "react-native";
+import {ScrollView, StyleSheet, View} from "react-native";
 import Index from "./components/Topo";
 import Detalhes from "./components/Detalhes";
+import Itens from "./components/Itens";
 
-export default function Cesta({topo, detalhes}) {
+export default function Cesta({topo, detalhes, itens}) {
     return (
-        <>
+        <ScrollView>
             <Index {...topo}/>
             <View style={estilos.cesta}>
                 <Detalhes {...detalhes}/>
+                <Itens {...itens}/>
             </View>
-        </>
+        </ScrollView>
     )
 }
 
