f34e459245e1cadd884bc60e45c4a294ee36db5a
Michel Marinho <michel.marinho04@outlook.com>
2022-12-23 10:19:02 -0400

    Add: Detalhes Components

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import {StatusBar, SafeAreaView, View} from 'react-native';
 import Cesta from "./src/pages/Cesta";
+import mock from './src/mocks/cesta';
 import {useFonts, Montserrat_400Regular, Montserrat_700Bold} from '@expo-google-fonts/montserrat';
 
 export default function App() {
@@ -16,7 +17,7 @@ export default function App() {
         <SafeAreaView>
             <View>
                 <StatusBar/>
-                <Cesta/>
+                <Cesta {...mock}/>
             </View>
         </SafeAreaView>
     );
--- src/pages/Cesta/components/Topo/index.js
+++ src/pages/Cesta/components/Topo/index.js
@@ -1,20 +1,20 @@
 import {Dimensions, Image, StyleSheet} from "react-native";
-import topo from "../../../../assets/topo.png";
-import Texto from "../../../components/texto";
+import topo from "../../../../../assets/topo.png";
+import Texto from "../../../../components/texto";
 
 const width = Dimensions.get('screen').width
 
-export default function Topo(){
-   return(
-       <>
-           <Image source={topo} style={estilos.topo}/>
-           <Texto style={estilos.titulo}>Detalhe da Cesta</Texto>
-       </>
-   )
+export default function Index({titulo}) {
+    return (
+        <>
+            <Image source={topo} style={estilos.topo}/>
+            <Texto style={estilos.titulo}>{titulo}</Texto>
+        </>
+    )
 }
 
 const estilos = StyleSheet.create({
-    topo:{
+    topo: {
         width: "100%",
         height: 578 / 768 * width,
         fontFamily: 'Montserrat'
